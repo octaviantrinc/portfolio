c43092fa2ccdf089d02e1d35a9a9086298b07090
octaviantrinc <59571712+octaviantrinc@users.noreply.github.com>
2020-02-27 14:10:05 +0000

    2nd Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <button></button>
+          <button></button>
+        </div>
       </header>
+      <body className="App-body">
+        <div>
+          <h1>Hi. Octavian here.</h1>
+          <h4>I am a professional full stack software developer.</h4>
+          <h4>Need some code? Have some questions?</h4>
+          <h4>Let's connect!</h4>
+        </div>
+        <div>
+          <h2>Skills</h2>
+        </div>
+        <div>
+          <h2>Work</h2>
+        </div>
+        <div>
+          <h2>About me</h2>
+        </div>
+      </body>
+      <footer className="App-footer">
+        <div>
+          <h2>Contact</h2>
+        </div>
+      </footer>
     </div>
   );
 }
