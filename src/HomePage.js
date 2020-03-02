81844ff074d48c64aa076249f3ae1fbeab4d2584
octaviantrinc <59571712+octaviantrinc@users.noreply.github.com>
2020-03-02 16:22:35 +0000

    Update icons

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,6 +1,22 @@
 import React from "react";
 import "./HomePage.css";
 import { Link } from "react-router-dom";
+import iconTwitter from './iconTwitter.svg';
+import iconEmail from './iconEmail.svg';
+import iconFacebook from './iconFacebook.svg';
+import iconWhatsapp from './iconWhatsapp.svg';
+import iconGithub from './iconGithub.svg';
+import iconLinkedin from './iconLinkedin.svg';
+import iconJavaScript from './iconJavaScript.svg';
+import iconReactJS from './iconReactJS.svg';
+import iconCss from './iconCss.svg';
+import iconHTML from './iconHTML.svg';
+import iconNodeJS from './iconNodeJS.svg';
+import iconGit from './iconGit.svg';
+import iconGA from './iconGA.svg';
+import iconSketch from './iconSketch.svg';
+
+//import iconX from './iconX';
 
 function HomePage() {
 
@@ -29,6 +45,26 @@ function HomePage() {
                     </div>
                     <div className="Skills">
                         <h2 className="skills font">Skills</h2>
+                        <div className="skillsIcons">
+                            <img className="iconJavaScript" src={iconJavaScript} alt="iconJavaScript" />
+                            <h5>JavaScript ES6</h5>
+                            <img className="iconReactJS" src={iconReactJS} alt="iconReactJS" />
+                            <h5>ReactJS</h5>
+                            <img className="iconHTML" src={iconHTML} alt="iconHTML" />
+                            <h5>HTML5</h5>
+                            <img className="iconCss" src={iconCss} alt="iconCss" />
+                            <h5>CSS3</h5>
+                            <img className="iconNodeJS" src={iconNodeJS} alt="iconNodeJS" />
+                            <h5>NodeJS</h5>
+                            <img className="iconGit" src={iconGit} alt="iconGit" />
+                            <h5>Git</h5>
+                            <img className="iconGithub" src={iconGithub} alt="iconGithub" />
+                            <h5>Github</h5>
+                            <img className="iconGA" src={iconGA} alt="iconGA" />
+                            <h5>Google Analytics</h5>
+                            <img className="iconSketch" src={iconSketch} alt="iconSketch" />
+                            <h5>Sketch</h5>
+                        </div>
                     </div>
                     <div className="Work">
                         <h2 className="work font">Works</h2>
@@ -40,9 +76,15 @@ function HomePage() {
                 <footer className="App-footer">
                     <div className="Contact">
                         <h2 className="contact font">Contact</h2>
+                        <div className="icons">
+                            <img className="iconC" src={iconWhatsapp} alt="iconWhatsapp" />
+                            <img className="iconC" src={iconEmail} alt="iconEmail" />
+                            <img className="iconC" src={iconGithub} alt="iconGithub" />
+                            <img className="iconC" src={iconLinkedin} alt="iconLinkedin" />
+                            <img className="iconC" src={iconTwitter} alt="iconTwitter" />
+                            <img className="iconC" src={iconFacebook} alt="iconFacebook" />
+                        </div>
                         <h5 className="low url">iamoctavian.com</h5>
-                    </div>
-                    <div>
                     </div>
                 </footer>
             </div>
